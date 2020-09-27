d070fba949b7a6ed94adf321c630dc1f239ba65e
ehsan1 <ehsantariq783@gmail.com>
2020-09-27 15:47:52 +0500

    Lecture 6

---
+++ Lecture 6 js 6 event binding/realExample.js
@@ -0,0 +1,6 @@
+function update(){
+    console.log("updating input");
+    var input = document.getElementById("myinput");
+    var output = document.getElementById("myoutput");
+    output.innerHTML = input.value;
+}
